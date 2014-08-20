f7da36eb9c221eb3433363395d0a5db804ab9877
SergeyLm <SergeyLm@mail.ru>
2014-08-20 23:13:41 +0300

    Commented 'protect_from_forgery' to have ability to run app locally

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  # protect_from_forgery
+  # skip_before_filter :verify_authenticity_token
 
-	def index
+  def index
 		gon.environment = Rails.env
 	end
 	
