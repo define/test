f773d832e77008f8c765558c690dd907a6567f77
SergeyLm <SergeyLm@mail.ru>
2014-08-20 23:00:17 +0300

    Added property: "modified_date"

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,5 @@
 class Todo < ActiveRecord::Base
-  attr_accessible :name, :is_completed, :priority, :created_date, :completed_date
+  attr_accessible :name, :is_completed, :priority, :created_date, :completed_date, :modified_date
 
 	# validates_presence_of :name
 	# validates_presence_of :is_completed, :priority, :created_date, :completed_date, :unless => proc { new_record? }
