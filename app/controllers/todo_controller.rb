cb3a5c7c308426f8350e492e250dbd7bde64dcc5
SergeyLm <SergeyLm@mail.ru>
2014-08-20 23:02:08 +0300

    Output in JSON are specified explicitly

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,22 +1,25 @@
 class TodoController < ApplicationController
 	respond_to :json
-	
+
 	def index
 		# sleep 5
 		@todo = Todo.all
+    render json: @todo
 	end
 	
 	def show
 		# sleep 5
 		@member = Todo.find params[:id]
+    render json: @member
 #		sleep 1
 	end
 	
 	def update
-		sleep 2
+		# sleep 2
 		@member = Todo.find params[:id]
 		if @member.update_attributes params
-			render "todo/show"
+			# render "todo/show"
+      render json: @member
 		else
 			respond_with @member
 		end
@@ -26,7 +29,8 @@ class TodoController < ApplicationController
 		sleep 2
 		@member = Todo.new
 		if @member.update_attributes params
-			render "todo/show"
+			# render "todo/show"
+      render json: @member
 		else
 			respond_with @member
 		end
